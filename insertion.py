23dcd200ef9c6f59fbbfc359b1ee96c86d73e88c
jd-develop <jd-dev@laposte.net>
2023-04-03 15:28:37 +0200

    update insertion.py

--- insertion.py
+++ insertion.py
@@ -15,6 +15,22 @@ def insertion_sort(target_list: list[int | float]):
     return target_list_
 
 
+def felix_insertion_sort(lst):
+    """Felix' insertion sort — thanks Felix!"""
+    lst = lst.copy()
+    for i in range(len(lst)):
+        insert_char(lst, i, lst[i])
+    return lst
+
+
+def insert_char(lst, i, v):
+    j = i
+    while j > 0 and lst[j-1] > v:
+        lst[j] = lst[j-1]
+        j = j-1
+    lst[j] = v
+
+
 # length = random.randint(5, 10)
 length = 100
 list_to_sort = [random.randint(-1000, 9999) for a in range(length)]
@@ -23,4 +39,4 @@ print(list_to_sort)
 sorted_ = insertion_sort(list_to_sort)
 print(sorted_)
 
-assert sorted_ == sorted(list_to_sort)
+assert sorted_ == sorted(list_to_sort) == felix_insertion_sort(list_to_sort)
